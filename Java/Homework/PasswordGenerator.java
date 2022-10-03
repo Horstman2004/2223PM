e9404f043a95b5f9ea8577862fa52ff4f6293b6b
Horstman2004 <adam.horstman@evsck12.com>
2022-10-03 14:05:39 -0500

    Java

---
+++ Java/Homework/PasswordGenerator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PasswordGenerator {
+    public static void main(String[] args){  
+        Scanner ui = new Scanner(System.in);  
+        int chars = 0;
+        int numbers = 0;
+        int specialChar = 0;
+        int total;
+        System.out.println("Enter how many characters you want.");
+        chars = ui.nextInt();
+        System.out.println("Enter how many numbers you want.");
+        numbers = ui.nextInt();
+        System.out.println("Enter how many special characters you want.");
+        specialChar = ui.nextInt();
+        total = chars+numbers+specialChar
+        if(chars > 0 && numbers > 0 && specialChar > 0 && total > 7);
+            passwordGenerate(chars,numbers,specialChar);
+        else(){
+            System.out.println("You need at least 8 total characters. You currently have: "+total);
+            main();
+        }
+
+    }
+
+    private static Integer PasswordGenerator(Integer chars, numbers, specialChar){
+        Int total = (chars+numbers+specialChar);
+        for(i=0;i>chars;i++){
+
+        }
+
+    }
+
+
+}
